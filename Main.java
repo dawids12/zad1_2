78b8a8fbb5d1621a2535253ca632cacabe2c0b94
dawids12 <101203650+dawids12@users.noreply.github.com>
2022-03-22 10:08:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b;
+    char c;
+    boolean d;
+    a = 2;
+    b = 3.14;
+    c = 's';
+    d = false;
+   System.out.println("int: " + a );
+    System.out.println("double: " + b );
+    System.out.println("char: " + c );
+    System.out.println("boolean: " + d );
+    
+  }
+}
